9a0205fced13dbfdb2848cb20e3d1ee05859b9cc
FuadTalukder85 <webfuadtk@gmail.com>
2023-12-22 02:07:12 +0600

    added footer

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,16 +1,12 @@
+import Footer from "../Shared/Footer/Footer";
+
 const Home = () => {
-    return (
-        <div>
-            <h1>This is home</h1>
-            <button className="btn">Button</button>
-            <button className="btn btn-neutral">Neutral</button>
-            <button className="btn btn-primary">Primary</button>
-            <button className="btn btn-secondary">Secondary</button>
-            <button className="btn btn-accent">Accent</button>
-            <button className="btn btn-ghost">Ghost</button>
-            <button className="btn btn-link">Link</button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is home</h1>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Pages/Home/Shared/Footer/Footer.jsx
@@ -0,0 +1,52 @@
+import logo from "../../../../image/logo/pikstack-logo.png";
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer p-10 text-base-content bg-[#F5F5DC] text-[#000000] font-semibold justify-items-center">
+        <nav className="w-40 flex items-center justify-center">
+          <img src={logo} alt="" />
+        </nav>
+        <nav>
+          <header className="footer-title">Services</header>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <header className="footer-title">Company</header>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <header className="footer-title">Legal</header>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+        <form>
+          <header className="footer-title">
+            Sign Up For Exclusive Deals & Offers
+          </header>
+          <fieldset className="form-control w-80">
+            <label className="label">
+              <span className="label-text">Enter your email address</span>
+            </label>
+            <div className="join">
+              <input
+                type="text"
+                placeholder="pikstack@site.com"
+                className="input input-bordered join-item"
+              />
+              <button className="btn btn-primary join-item">Subscribe</button>
+            </div>
+          </fieldset>
+        </form>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
