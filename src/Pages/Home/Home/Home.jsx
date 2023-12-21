840cf845c417e2a9934c496500ae7fdc1ccc415a
FuadTalukder85 <webfuadtk@gmail.com>
2023-12-22 02:50:54 +0600

    added navbar

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,10 @@
 import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <h1>This is home</h1>
       <Footer></Footer>
     </div>
