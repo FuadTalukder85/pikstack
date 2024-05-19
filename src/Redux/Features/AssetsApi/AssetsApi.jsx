efe3fe7a3b7b05800b6cb4625adbb37f1fd4f2a8
FuadTalukder85 <webfuadtk@gmail.com>
2024-05-19 23:00:32 +0600

    post assets

--- src/Redux/Features/AssetsApi/AssetsApi.jsx
+++ src/Redux/Features/AssetsApi/AssetsApi.jsx
@@ -2,6 +2,13 @@ import { baseApi } from "../../Api/BaseApi";
 
 const assetsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
+    postAssets: builder.mutation({
+      query: (postAssets) => ({
+        url: "/additem",
+        method: "POST",
+        body: postAssets,
+      }),
+    }),
     getAssets: builder.query({
       query: () => ({
         url: "/allitem",
@@ -16,4 +23,8 @@ const assetsApi = baseApi.injectEndpoints({
     }),
   }),
 });
-export const { useGetAssetsQuery, useGetAssetsByIdQuery } = assetsApi;
+export const {
+  usePostAssetsMutation,
+  useGetAssetsQuery,
+  useGetAssetsByIdQuery,
+} = assetsApi;
